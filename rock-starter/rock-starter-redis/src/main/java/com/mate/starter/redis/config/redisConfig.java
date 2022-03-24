b03c4beacb23476d9d478a2ad29d8ae0e0eb3c69
kevin <libwb@yonyou.com>
2022-03-24 20:00:28 +0800

    redis相关配置

---
+++ rock-starter/rock-starter-redis/src/main/java/com/mate/starter/redis/config/redisConfig.java
@@ -0,0 +1,11 @@
+package com.mate.starter.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Kevin on 2022/3/24 18:15
+ */
+@Configuration
+public class redisConfig {
+
+}
